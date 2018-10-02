7d5784dbc5c6a03287ef6311c76c35d545bd07cf
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-10-01 22:03:26 -0300

    added GET posts

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: 'ffas123', title: 'First server-side post', content: 'this is the first post\'s content' },
+    { id: 'sdsa212', title: 'Second server-side post', content: 'this is the second post\'s content' },
+    { id: 'asds312', title: 'Third server-side post', content: 'this is the third post\'s content' },
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;

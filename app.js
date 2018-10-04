7c222f0011a9e0bb88a342dfffaa7e9571f27bb9
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-10-03 22:37:50 -0300

    added get documents

--- app.js
+++ app.js
@@ -26,15 +26,19 @@ app.use((req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    { id: 'ffas123', title: 'First server-side post', content: 'this is the first post\'s content' },
-    { id: 'sdsa212', title: 'Second server-side post', content: 'this is the second post\'s content' },
-    { id: 'asds312', title: 'Third server-side post', content: 'this is the third post\'s content' },
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+  Post.find()
+    .then((documents) => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(404).json({
+        message: 'Posts can\'t be fetched'
+      });
+    });
 });
 
 app.post('/api/posts', (req, res, next) => {

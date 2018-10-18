05d688085cd378f7577fa563a4fd98bb68f01bf2
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-10-17 23:19:07 -0300

    add signup user endpoint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const postsRoutes = require('./routes/post');
+const userRoutes = require('./routes/user');
 
 const app = express();
 require('dotenv').config();
@@ -26,5 +27,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/posts', postsRoutes);
+app.use('/api/user', userRoutes);
 
 module.exports = app;
